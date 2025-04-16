f6a5931db9d3f5641e6f116c75ff8a576565f761
George Sidiras <georgesidiras@gmail.com>
2025-04-16 14:59:38 +0300

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LLms_intro
 The first steps to LLms
+
+We start with llama 3.2
+How we can use Local with python. Likewise make embending using Bert and llma3.2
---
+++ test.py
@@ -0,0 +1,21 @@
+from llama_cpp import Llama
+
+# Load the model but first you have to download
+llm = Llama(model_path="C:/LLMS_DEV/Model/Meta-Llama-3-8B-Instruct.Q2_K.gguf", n_ctx=2048)
+
+# Query the model
+response = llm("Q: What is the capital of France?\nA:", max_tokens=100)
+
+print(response["choices"][0]["text"].strip())
+
+ # this is the Embendings
+llm2 = Llama(
+    model_path="C:/LLMS_DEV/Model/Meta-Llama-3-8B-Instruct.Q2_K.gguf",
+    embedding=True  # 👈 Important!
+)
+
+text = "Air quality is important for health."
+embedding = llm2.embed(text)
+
+print(f"Embedding length: {len(embedding)}")
+print(embedding[:10])  # Preview first 10 dimensions
